9467f11c21745740cbe48fa94785ec09254f92f8
glubsy <glubsy@users.noreply.github.com>
2021-07-11 19:40:53 +0200

    Add fallback values for Windows
    * Still no idea how to query terminal capabilities on Windows.
    * Fall back to not adding special character sequences on that platform.
    * Build sequences that can take parameters (e.g. "cuu").
    * Small optimizations.

--- save_livestream.py
+++ save_livestream.py
@@ -7,33 +7,54 @@ from random import uniform
 
 MIN_WAIT = 45
 MAX_WAIT = 70
-TERM_CHARS = {
-    'el': '\33[K', # clr_eol, clear the line
-    'el1': '\33[2K', # clr_bol, clear to the beginning of line
-    'cuu1': '\033[A' # cursor_up, move cursor up
+TERM_SEQ = {}
+DEFAULT_TERM_SEQ = {
+    'el': '\33[K', # clr_eol, clear from the cursor to the end of the line
+    'el1': '\33[2K', # clr_bol, clear from the cursor to the beginning of the line
+                     # The <n> parameter has 3 possible values according to
+                     # https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#text-modification
+                     # so technically, this value is actually el2
+    'cuu1': '\033[A', # cursor_up, move cursor up by one line
+    'cuu2': '\033[2A'  # same as above, 2 lines
 }
-# print(f"DEBUG: Default special characters: {dict([(key, list(val)) for key, val in TERM_CHARS.items()])}")
 
-
-def get_terminal_char(char_type):
-    """Query terminfo string capabilities with tput for a non-printable 
+def get_term_seq(char_type):
+    """Query terminfo string capabilities with tput for a non-printable
     character that can be used by the current terminal (see man 5 terminfo)."""
     try:
         proc = subprocess.run(["tput", char_type],
             capture_output=True, check=True, text=True
         )
-        ret = proc.stdout
-        return ret
+        return proc.stdout  # or stdout.decode() if text=False
     except subprocess.CalledProcessError as e:
-        print(f"Error getting capability \"{char_type}\" from tput: {e}")
-    return None
+        print(f"Error getting capability \"{char_type}\" for this terminal: {e}")
+    return ""
 
+# Build sequences ahead of time
+for key, value in DEFAULT_TERM_SEQ.items():
 
-for key, value in TERM_CHARS.items():
-    char = get_terminal_char(key)
-    if char is not None:
-        TERM_CHARS[key] = char
-# print(f"DEBUG: Updated special characters: {dict([(key, list(val)) for key, val in TERM_CHARS.items()])}")
+    # Fallback, because not sure how to query capabilities on Windows
+    if sys.platform == "win32":
+        TERM_SEQ[key] = ""
+        continue
+
+    # Special key. Sequences with parameters (ie. "2") are not retrievable
+    if key == 'cuu2':
+        if TERM_SEQ.get('cuu1') is not None:  # we don't have this key yet
+            # Insert "2" parameter in the seq
+            idx = TERM_SEQ['cuu1'].rfind('A')
+            if idx == -1:
+                TERM_SEQ[key] = TERM_SEQ['cuu1'] + TERM_SEQ['cuu1']
+                continue
+            TERM_SEQ['cuu2'] = TERM_SEQ['cuu1'][:idx] + "2" + TERM_SEQ['cuu1'][idx:]
+            continue
+        TERM_SEQ[key] = DEFAULT_TERM_SEQ['cuu2']
+        continue
+
+    TERM_SEQ[key] = get_term_seq(key)
+
+# print(f"Default key sequences: {dict([(key, list(val)) for key, val in DEFAULT_TERM_SEQ.items()])}")
+# print(f"Updated key sequences: {dict([(key, list(val)) for key, val in TERM_SEQ.items()])}")
 
 
 def download(link, quality="best"):
@@ -60,8 +81,14 @@ def download(link, quality="best"):
         # print(f"{cmd[0]} exit code: \"{e.returncode}\"")
 
         # Overwrite the two last lines with current time to reduce backlog clutter
-        sys.stdout.write(TERM_CHARS['cuu1'])
-        sys.stdout.write(TERM_CHARS['cuu1'] + TERM_CHARS['el1'] + '\r' + current_date_time + " " + TERM_CHARS['el'])
+        sys.stdout.write(
+            TERM_SEQ['cuu2']
+            + TERM_SEQ['el1']
+            + '\r'
+            + current_date_time
+            + " "
+            + TERM_SEQ['el']
+        )
         # sys.stdout.flush()
 
 
